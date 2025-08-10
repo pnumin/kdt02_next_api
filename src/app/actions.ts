d6c32c294873cad23e3989915bae3cfc76fc65ee
pnumin <pnumin@pusan.ac.kr>
2025-08-10 19:02:41 +0900

    상품 CRUD

---
+++ src/app/actions.ts
@@ -0,0 +1,63 @@
+'use server'
+
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+
+export interface FormStatus {
+  message: string;
+}
+
+export async function createProductAction(prevState: FormStatus, formData: FormData): Promise<FormStatus> {
+  const name = formData.get('name');
+  const category = formData.get('category');
+  const price = formData.get('price');
+  const description = formData.get('description');
+
+  console.log('createProductAction', name)
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const resp = await fetch(`${baseUrl}/api/products`, {
+    method: "POST",
+    headers : {
+      'Content-type' : 'application/json'
+    },
+    body:JSON.stringify({name,category,price,description })
+  });
+
+  if (!resp.ok) {
+    const data = resp.json() ;
+    return {message : `API오류 : ${data}`}
+  }
+
+  // 캐시를 갱신하여 새데이터 즉시 반영
+  revalidatePath('/product')
+  //제품목록 페이지로 이동
+  redirect('/product')
+}
+
+export async function updateProductAction(prevState: FormStatus, formData: FormData): Promise<FormStatus> {
+  const id = formData.get('id');
+  const name = formData.get('name');
+  const category = formData.get('category');
+  const price = formData.get('price');
+  const description = formData.get('description');
+
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const resp = await fetch(`${baseUrl}/api/products/${id}`, {
+    method: "PUT",
+    headers : {
+      'Content-type' : 'application/json'
+    },
+    body:JSON.stringify({name,category,price,description })
+  });
+
+  if (!resp.ok) {
+    const data = resp.json() ;
+    return {message : `API오류 : ${data}`}
+  }
+
+  // 캐시를 갱신하여 새데이터 즉시 반영
+  revalidatePath('/product')
+  revalidatePath(`/product/${id}`)
+  //제품목록 페이지로 이동
+  redirect(`/product/${id}`)
+}
---
+++ src/app/product/ProductCard.tsx
@@ -0,0 +1,24 @@
+import type { Product } from "@/types/product";
+import Link from "next/link";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export default async function ProductCard({product}:ProductCardProps) {
+  return (
+    <div className="border border-gray-400 p-4 rounded-lg">
+      <h2 className="text-2xl font-bold my-4">
+        <Link href={`/product/${product.id}`}>
+          {product.name}
+        </Link> 
+      </h2>
+      <p className="text-gray-500">
+        {product.description}
+      </p>
+      <p className="text-2xl font-bold text-blue-700 text-right">
+        {product.price.toLocaleString()} 원
+      </p>
+    </div>
+  );
+}
